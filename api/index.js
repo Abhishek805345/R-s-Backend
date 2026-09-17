dd82d03e0e5a0ed4fb3235b90fc313f146b5903d
Abhishek805345 <jakharabhi49@gmail.com>
2026-09-17 19:58:46 +0530

    d

---
+++ api/index.js
@@ -0,0 +1,9 @@
+import { createApp } from "../src/app.js";
+import { connectDatabase } from "../src/config/database.js";
+
+const app = createApp();
+
+export default async function handler(req, res) {
+  await connectDatabase();
+  return app(req, res);
+}
--- src/config/database.js
+++ src/config/database.js
@@ -2,7 +2,13 @@ import mongoose from "mongoose";
 import { MONGODB_URI } from "./env.js";
 
 export async function connectDatabase() {
+  if (mongoose.connection.readyState === 1) {
+    return mongoose.connection;
+  }
+
   mongoose.set("strictQuery", true);
   await mongoose.connect(MONGODB_URI);
   console.log("database connected");
+
+  return mongoose.connection;
 }
